a894279c88a46b76b1c7b916f76da9aae80e4f72
Manolis Liolios <manos@mystenlabs.com>
2023-09-08 20:27:01 +0300

    Only keep fields that are valid for the coupon in the ruleset

--- scripts/coupons/query.ts
+++ scripts/coupons/query.ts
@@ -1,4 +1,5 @@
 import { PackageInfo, mainPackage } from "../config/constants";
+import { CouponRules } from "./coupon";
 
 export enum CouponTypes {
     PERCENTAGE,
@@ -32,22 +33,28 @@ export const getCouponByCodeName = async (name: string, config: PackageInfo): Pr
     // @ts-ignore-next-line
     let parsedData = coupon.data?.content?.fields?.value?.fields;
 
+    const rules: CouponRules = {}
+
+    let ruleset = parsedData.rules.fields;
+
+    if(ruleset.available_claims) rules.availableClaims = Number(ruleset.available_claims);
+    if(ruleset.expiration) rules.expiration = ruleset.expiration;
+    if(ruleset.user) rules.user = ruleset.user;
+    if(ruleset.length?.fields.vec) rules.length = ruleset.length.fields.vec;
+    if(ruleset.years?.fields.vec) rules.years = ruleset.years.fields.vec;
+
+
     let couponObject: CouponInfo = {
         name,
         amount: parsedData.amount, 
         type: parsedData.type === 0 ? CouponTypes.PERCENTAGE : CouponTypes.FIXED,
-        rules: {
-            availableClaims: parsedData.rules.fields.available_claims ? Number(parsedData.rules.fields.available_claims) : undefined,
-            expiration: parsedData.rules.fields.expiration ?? undefined,
-            user: parsedData.rules.fields.user ?? undefined,
-            length: parsedData.rules.fields.length?.fields.vec,
-            years: parsedData.rules.fields.years?.fields.vec
-        }
+        rules
     }
 
     if(couponObject.rules.expiration){
         couponObject.rules.expirationDate = new Date(Number(couponObject.rules.expiration))
     }
+    console.log(couponObject);
     return couponObject;
 }
 
