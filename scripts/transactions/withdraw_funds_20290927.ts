f2d5c85dc086acee96ec1c4cdbaca1042df5c524
Manolis Liolios <manos@mystenlabs.com>
2023-09-27 17:18:20 +0300

    Add tx & gh action + move airdrop data to separate folder to make action data smaller

---
+++ scripts/transactions/withdraw_funds_20290927.ts
@@ -0,0 +1,31 @@
+// Copyright (c) 2023, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { TransactionBlock } from '@mysten/sui.js';
+import { mainPackage } from '../config/constants';
+import { prepareMultisigTx } from '../airdrop/helper';
+
+/// MystenLabs treasury address.
+const ADDRESS_TO_TRANSFER_FUNDS =
+	'0x638791b625c4482bc1b917847cdf8aa76fe226c0f3e0a9b1aa595625989e98a1';
+
+const craftTx = async () => {
+	const txb = new TransactionBlock();
+	const config = mainPackage.mainnet;
+
+	const adminCapObj = txb.object(config.adminCap);
+
+	// Auction house profits.
+	const auctionProfits = txb.moveCall({
+		target: `${config.packageId}::auction::admin_withdraw_funds`,
+		arguments: [adminCapObj, txb.object('0x2588e11685b460c725e1dc6739a57c483fcd23977369af53d432605225e387f9')],
+	});
+
+	txb.transferObjects(
+		[auctionProfits],
+		txb.pure(ADDRESS_TO_TRANSFER_FUNDS, 'address'),
+	);
+	await prepareMultisigTx(txb, 'mainnet');
+};
+
+craftTx();
