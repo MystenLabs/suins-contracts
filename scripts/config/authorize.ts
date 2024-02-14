62c2fdfdb1a9354f80051a604d086fb03d096de8
Manolis Liolios <manos@mystenlabs.com>
2024-02-14 15:46:12 +0200

    Renewals + scripts

---
+++ scripts/config/authorize.ts
@@ -0,0 +1,36 @@
+import { TransactionBlock } from "@mysten/sui.js";
+import { Network, mainPackage } from "../config/constants";
+
+/** 
+ * Default code for authorizing an app on SuiNS
+ * `app` should be in the type of `packageId::app::AppName`
+ * e..g `0x701b8ca1c40f11288a1ed2de0a9a2713e972524fbab748a7e6c137225361653f::renew::Renew`
+ */
+export const authorizeApp = async (network: Network, txb: TransactionBlock, app: string) => {
+    const suinsPackageConfig = mainPackage[network];
+
+    txb.moveCall({
+        target: `${suinsPackageConfig.packageId}::suins::authorize_app`,
+        arguments: [
+          txb.object(suinsPackageConfig.adminCap),
+          txb.object(suinsPackageConfig.suins),
+        ],
+        typeArguments: [app],
+    });
+}
+
+/** Default code for de-authorizing an app on SuiNS */
+export const deauthorizeApp = async (network: Network, txb: TransactionBlock, app: string): Promise<TransactionBlock | void> => {
+    const suinsPackageConfig = mainPackage[network];
+
+    txb.moveCall({
+        target: `${suinsPackageConfig.packageId}::suins::deauthorize_app`,
+        arguments: [
+          txb.object(suinsPackageConfig.adminCap),
+          txb.object(suinsPackageConfig.suins),
+        ],
+        typeArguments: [app],
+    });
+
+    return txb;
+}
---
+++ scripts/transactions/renewals/authorize_renewals.ts
@@ -0,0 +1,24 @@
+// Copyright (c) 2023, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import dotenv from "dotenv";
+dotenv.config();
+import { TransactionBlock } from "@mysten/sui.js";
+import { authorizeApp } from "../../config/authorize";
+import { mainPackage } from "../../config/constants";
+import { prepareMultisigTx } from "../../airdrop/helper";
+
+const gasObject = process.env.GAS_OBJECT;
+
+// Github actions are always on mainnet.
+const execute = async () => {
+    if(!gasObject) throw new Error("Gas Object not supplied for a mainnet transaction");
+    const txb = new TransactionBlock();
+    const config = mainPackage.mainnet;
+
+    authorizeApp('mainnet', txb, `${config.renewalsPackageId}::renew::Renew`);
+
+    prepareMultisigTx(txb, 'mainnet');
+}
+
+execute();
