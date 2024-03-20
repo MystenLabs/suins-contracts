c504aa20c089f78242b003f4ccda1cddb9a03522
Manolis Liolios <manos@mystenlabs.com>
2024-03-20 15:21:59 +0200

    Update constants.ts

--- suins-sdk/src/constants.ts
+++ suins-sdk/src/constants.ts
@@ -29,7 +29,7 @@ export const TESTNET_CONFIG: Constants = {
     suinsObjectId: '0x300369e8909b9a6464da265b9a5a9ab6fe2158a040e84e808628cde7a07ee5a3',
     utilsPackageId: '0x6ed81fd808a23eae2da488052334d50478b36527474fc99707c1aed0e43104b1',
     registrationPackageId: '0x4255184a0143c0ce4394a3f16a6f5aa5d64507269e54e51ea396d569fe8f1ba5',
-    renewalPackageId: 'renewal',
+    renewalPackageId: '0x54800ebb4606fd0c03b4554976264373b3374eeb3fd63e7ff69f31cac786ba8c',
     subNamesPackageId: '0x3c272bc45f9157b7818ece4f7411bdfa8af46303b071aca4e18c03119c9ff636',
     tempSubNamesProxyPackageId: '0x3489ab5dcd346afee8b681267bcab2583a5eba9855680ec9931355e50e21c148',
     registryTableId: '0xb120c0d55432630fce61f7854795a3463deb6e3b443cc4ae72e1282073ff56e4'
