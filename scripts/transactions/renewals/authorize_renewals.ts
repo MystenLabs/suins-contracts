8ea9ee23f2ed02a6765dde80a13ebf277dc7ef21
Manolis Liolios <manos@mystenlabs.com>
2024-04-20 10:55:06 +0300

    nit

--- scripts/transactions/renewals/authorize_renewals.ts
+++ scripts/transactions/renewals/authorize_renewals.ts
@@ -7,39 +7,37 @@ import { MIST_PER_SUI } from "@mysten/sui.js/utils";
 import { Network, mainPackage } from "../../config/constants";
 import { TransactionBlock } from "@mysten/sui.js/transactions";
 import { prepareMultisigTx, signAndExecute } from "../../utils/utils";
+import { authorizeApp, newPriceConfig, setupApp } from "../../init/authorization";
 
 export const authorize = async (network: Network) => {
     const txb = new TransactionBlock();
-
     const config = mainPackage[network];
-    txb.moveCall({
-        target: `${config.packageId}::suins::authorize_app`,
-        arguments: [
-          txb.object(config.adminCap),
-          txb.object(config.suins),
-        ],
-        typeArguments: [`${config.renewalsPackageId}::renew::Renew`],
-      });
-    
-      const configuration = txb.moveCall({
-        target: `${config.packageId}::config::new`,
-        arguments: [
-          txb.pure([...Array(33).keys()]),
-          txb.pure(50n * MIST_PER_SUI),
-          txb.pure(10n * MIST_PER_SUI),
-          txb.pure(2n * MIST_PER_SUI),
-        ],
-      });
 
-      txb.moveCall({
+    authorizeApp({
+        txb,
+        adminCap: config.adminCap,
+        suins: config.suins,
+        type: `${config.renewalsPackageId}::renew::Renew`,
+        suinsPackageIdV1: config.packageId
+    });
+
+    const configuration = newPriceConfig({
+        txb,
+        suinsPackageIdV1: config.packageId,
+        priceList: {
+            three: 50 * Number(MIST_PER_SUI),
+            four: 10 * Number(MIST_PER_SUI),
+            fivePlus: 2 * Number(MIST_PER_SUI)
+        }
+    });
+  
+    setupApp({
+        txb,
+        adminCap: config.adminCap,
+        suins: config.suins,
         target: `${config.renewalsPackageId}::renew::setup`,
-        arguments: [
-          txb.object(config.adminCap),
-          txb.object(config.suins),
-          configuration,
-        ],
-        typeArguments: [],
-      });
+        args: [configuration]
+    });
     
     // for mainnet, we just prepare multisig TX
     if(network === 'mainnet') return prepareMultisigTx(txb, 'mainnet');
