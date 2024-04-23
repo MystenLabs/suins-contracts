d5790c822108c6f92634ff56356ca8f4b4400217
Manolis Liolios <manos@mystenlabs.com>
2024-04-23 10:17:13 +0300

    nit

--- documentation/components/nav.js
+++ documentation/components/nav.js
@@ -7,7 +7,7 @@ const NavBar = () => {
     const themeSwitch = useConfig().themeSwitch.component({lite: false, className: 'bg-white'});
 
     return (
-        <div className="suins-nav-container sticky top-0 w-full bg-blurple z-10">
+        <div className="suins-nav-container sticky top-0 text-white w-full bg-blurple z-10">
             <nav className="max-w-[110rem] grid grid-cols-12 h-16 gap-4 items-center">
                 <div className="col-span-3 ml-2">
                     <Link href="/">
@@ -15,11 +15,11 @@ const NavBar = () => {
                         <h1 className="text-white bold hidden lg:inline">{logo}</h1>
                     </Link>
                 </div>
-                <div className="col-span-9 flex justify-end mr-8 items-center">
+                <div className="col-span-9 flex  justify-end mr-8 items-center">
                     <ul>
                         <li>
                             <a href="https://suins.io/account/my-names" target="_blank">
-                                <span className="external-link text-xs">
+                                <span className="external-link text-xs ">
                                     <span className='hidden sm:inline'>SuiNS </span>
                                     Dashboard
                                 </span>
