4049e0d477dcdf20478d47c8845495fa1ac2ecd6
Manolis Liolios <manos@mystenlabs.com>
2024-05-14 15:39:31 +0300

    Update to sdk 0.54.1, lint

--- scripts/init/queries.ts
+++ scripts/init/queries.ts
@@ -1,20 +1,24 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+import { SuiClient } from '@mysten/sui.js/client';
 
-import { SuiClient } from "@mysten/sui.js/client";
+export const queryRegistryTable = async (
+	client: SuiClient,
+	suins: string,
+	suinsPackageId: string,
+) => {
+	const table = await client.getDynamicFieldObject({
+		parentId: suins,
+		name: {
+			type: `${suinsPackageId}::suins::RegistryKey<${suinsPackageId}::registry::Registry>`,
+			value: {
+				dummy_field: false,
+			},
+		},
+	});
 
-export const queryRegistryTable = async (client: SuiClient, suins: string, suinsPackageId: string) => {
+	if (table.data?.content?.dataType !== 'moveObject') throw new Error('Invalid data');
 
-    const table = await client.getDynamicFieldObject({
-        parentId: suins,
-        name: {
-            type: `${suinsPackageId}::suins::RegistryKey<${suinsPackageId}::registry::Registry>`,
-            value: {
-                dummy_field: false
-            }
-        }
-    });
-
-    if (table.data?.content?.dataType !== 'moveObject') throw new Error("Invalid data");
-
-    const data = table.data?.content.fields as Record<string, any>;
-    return data.value.fields.registry.fields.id.id;
-}
+	const data = table.data?.content.fields as Record<string, any>;
+	return data.value.fields.registry.fields.id.id;
+};
