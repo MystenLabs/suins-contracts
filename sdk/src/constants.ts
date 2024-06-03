027473b975010be2fb88b1f636bb3a86c5ed6d28
Manolis Liolios <manos@mystenlabs.com>
2024-06-03 11:40:00 +0300

    Adds missing function in temp subname proxy, publishes on mainnet/testnet

--- sdk/src/constants.ts
+++ sdk/src/constants.ts
@@ -31,7 +31,7 @@ export const MAINNET_CONFIG: Constants = {
 	registrationPackageId: '0x9d451fa0139fef8f7c1f0bd5d7e45b7fa9dbb84c2e63c2819c7abd0a7f7d749d',
 	renewalPackageId: '0xd5e5f74126e7934e35991643b0111c3361827fc0564c83fa810668837c6f0b0f',
 	subNamesPackageId: '0xe177697e191327901637f8d2c5ffbbde8b1aaac27ec1024c4b62d1ebd1cd7430',
-	tempSubNamesProxyPackageId: '0x59effd9323a836dd7373108ce1ca93acd8570c5d28a2ce790e5bfe34b649a0d0',
+	tempSubNamesProxyPackageId: '0xdacfd7c1176a68137b38a875d76a2f65d277596d2c632881931d926b16de2698',
 	registryTableId: '0xe64cd9db9f829c6cc405d9790bd71567ae07259855f4fba6f02c84f52298c106',
 };
 
@@ -45,6 +45,6 @@ export const TESTNET_CONFIG: Constants = {
 	registrationPackageId: '0x4255184a0143c0ce4394a3f16a6f5aa5d64507269e54e51ea396d569fe8f1ba5',
 	renewalPackageId: '0x54800ebb4606fd0c03b4554976264373b3374eeb3fd63e7ff69f31cac786ba8c',
 	subNamesPackageId: '0x3c272bc45f9157b7818ece4f7411bdfa8af46303b071aca4e18c03119c9ff636',
-	tempSubNamesProxyPackageId: '0xd258bfc8d5a58f18deaf069e758a66200eb09614566cc0ee66bbe17a261d2206',
+	tempSubNamesProxyPackageId: '0xfd5ad004acbd5e3dd4fc0de4f1f1d465f8db5bb2ec1de63694ce6dc887fe1c89',
 	registryTableId: '0xb120c0d55432630fce61f7854795a3463deb6e3b443cc4ae72e1282073ff56e4',
 };
