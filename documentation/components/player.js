cbd3a87ba352b69a71a8235e825875d367caa92b
Ronny Roland <ronmilton@mystenlabs.com>
2024-06-21 11:08:42 -0600

    Updated docs

---
+++ documentation/components/player.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import ReactPlayer from 'react-player/lazy'
+
+const Player = ({ videoFilename }) => {
+  const [hasWindow, setHasWindow] = useState(false);
+  const [duration, setDuration] = useState();
+  const videoUrl = `/videos/${videoFilename.replace('.mp4', '')}.mp4`;
+  const thumbnailUrl = `/videos/thumbnails/${videoFilename.replace('.mp4', '')}.png`;
+
+  const handleDuration = (time) => {
+    setDuration(time);
+  }
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
+
+  return (
+    <div>
+    <p className="py-4">Watch this quick {duration && <span>{Math.floor(duration)} second</span>} clip to see how.</p>
+    <div className='relative pt-[56.25%] p-4 m-4 border'>
+      {hasWindow && <ReactPlayer
+        className='absolute top-0 left-0'
+        url={videoUrl}
+        controls={true}
+        light={thumbnailUrl}
+        playing={true}
+        width='100%'
+        height='100%'
+        onDuration={handleDuration}
+      />}
+    </div>
+    </div>
+  )
+}
+
+  export default Player;
