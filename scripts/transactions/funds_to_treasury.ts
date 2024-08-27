5c8940b4f1a4b5cf200dcdfab052e53932b113df
Manolis Liolios <manos@mystenlabs.com>
2024-08-27 13:22:56 +0300

    remove auction from funds to treasury script, since we're draining it now

--- scripts/transactions/funds_to_treasury.ts
+++ scripts/transactions/funds_to_treasury.ts
@@ -12,24 +12,12 @@ const craftTx = async () => {
 
 	const adminCapObj = txb.object(config.adminCap);
 
-	// Auction house profits.
-	const auctionProfits = txb.moveCall({
-		target: `${config.packageId}::auction::admin_withdraw_funds`,
-		arguments: [
-			adminCapObj,
-			txb.object('0x2588e11685b460c725e1dc6739a57c483fcd23977369af53d432605225e387f9'),
-		],
-	});
-
 	const generalProfits = txb.moveCall({
 		target: `${config.packageId}::suins::withdraw`,
 		arguments: [adminCapObj, txb.object(config.suins)],
 	});
 
-	txb.transferObjects(
-		[auctionProfits, generalProfits],
-		txb.pure(config.treasuryAddress, 'address'),
-	);
+	txb.transferObjects([generalProfits], txb.pure(config.treasuryAddress, 'address'));
 	await prepareMultisigTx(txb, 'mainnet', config.adminAddress);
 };
 
