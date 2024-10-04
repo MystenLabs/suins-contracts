9ee4a9194384681e601dfa85faf7cfa66aa553e5
Tony Lee <tony.lee@mystenlabs.com>
2024-10-04 11:47:26 -0400

    wUSDC/USDC pool (Deepbook V2) (#178)
    * wusdc usdc pool
    
    * move name
    
    * move action
    
    * switch gas object
    
    * update gas object
    
    * admin address

--- scripts/transactions/deepbook/create_pools.ts
+++ scripts/transactions/deepbook/create_pools.ts
@@ -4,22 +4,15 @@
 import { TransactionBlock } from '@mysten/sui.js/transactions';
 import dotenv from 'dotenv';
 
-import { Network } from '../../config/constants';
+import { Network, mainPackage } from '../../config/constants';
 import { prepareMultisigTx, signAndExecute } from '../../utils/utils';
 
 dotenv.config();
 
-const SUI = '0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI';
-const USDY = '0x960b531667636f39e85867775f52f6b1f220a058c4de786905bdf761e06a56bb::usdy::USDY';
-const USDC = '0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN';
-// const WUSDCETH = '0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN';
-// const WBTC = '0xbc3a676894871284b3ccfb2eec66f428612000e2a6e6d23f592ce8833c27c973::coin::COIN';
-// const WETH = '0xaf8cd5edc19c4512f4259f0bee101a40d41ebed738ade5874359610ef8eeced5::coin::COIN';
-// const USDT = '0xc060006111016b8a020ad5b33834984a437aaa7d3c74c18e09a95d48aceab08c::coin::COIN';
-// const TESTNET_COIN =
-// 	'0x0c5f16ebb22a354ccb8f4dc163df0e729d0d37b565b4178046ea342ea0a93391::gold::GOLD';
-// const TESTNET_SUI = '0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI';
-// const TESTNET_DAI = '0x700de8dea1aac1de7531e9d20fc2568b12d74369f91b7fad3abc1c4f40396e52::dai::DAI';
+// const SUI = '0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI';
+// const USDY = '0x960b531667636f39e85867775f52f6b1f220a058c4de786905bdf761e06a56bb::usdy::USDY';
+const WUSDC = '0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN';
+const USDC = '0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC';
 
 export const CREATION_FEE = 100 * 1e9;
 export const PACKAGE_ID = '0xdee9';
@@ -40,7 +33,7 @@ const setup = async (network: Network) => {
 
 	// Create USDY / USDC
 	txb.moveCall({
-		typeArguments: [USDY, USDC],
+		typeArguments: [WUSDC, USDC],
 		target: `${PACKAGE_ID}::${MODULE_CLOB}::create_customized_pool`,
 		arguments: [
 			txb.pure(100000), // tick
@@ -51,23 +44,8 @@ const setup = async (network: Network) => {
 		],
 	});
 
-	const [coin2] = txb.splitCoins(txb.gas, [txb.pure(CREATION_FEE)]);
-
-	// Create SUI / USDY
-	txb.moveCall({
-		typeArguments: [SUI, USDY],
-		target: `${PACKAGE_ID}::${MODULE_CLOB}::create_customized_pool`,
-		arguments: [
-			txb.pure(100), // tick
-			txb.pure(100000000), // lot
-			txb.pure(DEFAULT_TAKER_FEE), // taker fee
-			txb.pure(DEFAULT_MAKER_FEE), // maker rebate
-			coin2, // creation fee
-		],
-	});
-
 	// for mainnet, we prepare the multi-sig tx.
-	if (network === 'mainnet') return prepareMultisigTx(txb, 'mainnet');
+	if (network === 'mainnet') return prepareMultisigTx(txb, 'mainnet', mainPackage.mainnet.adminAddress);
 
 	// For testnet, we execute the TX directly.
 	return signAndExecute(txb, network);
