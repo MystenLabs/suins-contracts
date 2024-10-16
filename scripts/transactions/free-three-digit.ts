ad8b3933083716966b91800e798735e57ebd7f99
Manolis Liolios <manos@mystenlabs.com>
2024-10-16 17:43:30 +0300

    Create free coupons (#182)

---
+++ scripts/transactions/free-three-digit.ts
@@ -0,0 +1,34 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+
+import { mainPackage } from '../config/constants';
+import { PercentageOffCoupon } from '../coupons/coupon';
+import { prepareMultisigTx } from '../utils/utils';
+import addresses from './addresses.json';
+
+/// Issue free claim coupon for anyone who purchased 3-digit names
+/// with full price.
+const prepareCoupons = async () => {
+	const pkg = mainPackage.mainnet;
+	const tx = new TransactionBlock();
+
+	const expiration = `1760691600000`; // 2025 Oct 17 09:00:00 UTC
+
+	// Create a free claim coupon for each wallet
+	for (const wallet of addresses) {
+		let coupon = new PercentageOffCoupon(100);
+
+		coupon.setName(`free-claim-${wallet}`);
+
+		coupon.setExpiration(expiration);
+		coupon.setUser(wallet);
+		coupon.setAvailableClaims(1);
+		coupon.setYears([1, 1]);
+		coupon.toTransaction(tx, pkg);
+	}
+
+	await prepareMultisigTx(tx, 'mainnet', pkg.adminAddress);
+};
+
+prepareCoupons();
