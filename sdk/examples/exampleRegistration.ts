9fffbed49949a8cf78450d98dd00a4f7f394aa81
Tony Lee <chunglee2018@u.northwestern.edu>
2024-12-17 13:15:27 -0500

    set target

--- sdk/examples/exampleRegistration.ts
+++ sdk/examples/exampleRegistration.ts
@@ -21,15 +21,24 @@ import { SuinsTransaction } from '../src/suins-transaction';
 		network,
 	});
 
-	const suinsTx = new SuinsTransaction(suinsClient, new Transaction());
-	const nft = await suinsTx.register('manosmassssnos.sui', 1, suinsClient.config.coins.NS, {
-		couponCode: 'fiveplus15percentoff',
-		coinId: '0x32f09a77fc392453cf412b65c5a9a40134cf72d5b3206b2df6d311e032e1613e',
-	});
+	// console.log(await suinsClient.getCoinTypeDiscount());
+	// console.log(await suinsClient.getNameRecord('tonytony.sui'));
 
-	if (nft) {
-		suinsTx.transaction.transferObjects([nft], suinsTx.getActiveAddress());
-	}
+	const suinsTx = new SuinsTransaction(suinsClient, new Transaction());
+
+	/* Register a new SuiNS name */
+	// const nft = await suinsTx.register('tonytony.sui', 1, suinsClient.config.coins.NS, {
+	// 	couponCode: 'fiveplus15percentoff',
+	// 	coinId: '0x32f09a77fc392453cf412b65c5a9a40134cf72d5b3206b2df6d311e032e1613e',
+	// });
+	// if (nft) {
+	// 	suinsTx.transaction.transferObjects([nft], suinsTx.getActiveAddress());
+	// }
+
+	/* Set target address */
+	const nft = '0x42004431a99f4467a892815f3d8fed20c4396256332ead3b6687a36219b18758';
+	const address = '0xe55684e6a538d29df4b9f510a6bd3ec17d3dc47fb524839954bf7fcf835b5d0d';
+	suinsTx.setTargetAddress({ nft, address });
 
 	return suinsTx.signAndExecute();
 })();
