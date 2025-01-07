dc4091f04e7a3ed86ceb3d2c146b536ec04e3b96
Tony Lee <chunglee2018@u.northwestern.edu>
2025-01-07 16:39:12 -0500

    test

--- sdk/examples/exampleRegistration.ts
+++ sdk/examples/exampleRegistration.ts
@@ -24,32 +24,32 @@ import { SuinsTransaction } from '../src/suins-transaction';
 	// console.log(await suinsClient.getCoinTypeDiscount());
 	// console.log('asd2.asdasd1.sui', await suinsClient.getNameRecord('asd2.asdasd1.sui'));
 	// console.log('asdasd1.sui', await suinsClient.getNameRecord('asdasd1.sui'));
-	console.log(await suinsClient.getNameRecord('testingtesting.sui'));
-	console.log(await suinsClient.getNameRecord('testt.testingtesting.sui'));
+	// console.log(await suinsClient.getNameRecord('testingtesting.sui'));
+	// console.log(await suinsClient.getNameRecord('testt.testingtesting.sui'));
 
-	// const suinsTx = new SuinsTransaction(suinsClient, new Transaction());
+	const suinsTx = new SuinsTransaction(suinsClient, new Transaction());
 
 	/* Register a new SuiNS name */
-	// const nft = await suinsTx.register('testingtesting.sui', 1, suinsClient.config.coins.NS, {
-	// 	couponCode: 'fiveplus15percentoff',
-	// 	coinId: '0x32f09a77fc392453cf412b65c5a9a40134cf72d5b3206b2df6d311e032e1613e',
-	// });
+	const nft = await suinsTx.register('testisssngtesting.sui', 1, suinsClient.config.coins.USDC, {
+		// couponCode: 'fiveplus15percentoff',
+		coinId: '0x3a6f32a201ad7f7491a56a1f513c97ec24995494019ba02a586fa181c5d266c5',
+	});
 
-	// if (nft) {
-	// 	/* Set target address */
-	// 	// const nft = '0x1f7c9f5c34b43ce71cf2a5bb42f0b91dc7971ef0256bb6dea28a33bd9ab4cfe1';
-	// 	const address = '0xb3d277c50f7b846a5f609a8d13428ae482b5826bb98437997373f3a0d60d280e';
-	// 	suinsTx.setTargetAddress({ nft, address });
+	if (nft) {
+		/* Set target address */
+		// const nft = '0x1f7c9f5c34b43ce71cf2a5bb42f0b91dc7971ef0256bb6dea28a33bd9ab4cfe1';
+		const address = '0xb3d277c50f7b846a5f609a8d13428ae482b5826bb98437997373f3a0d60d280e';
+		// suinsTx.setTargetAddress({ nft, address });
 
-	// 	suinsTx.setDefault('tonysstddondytony.sui');
-	// 	suinsTx.setUserData({
-	// 		nft,
-	// 		value: 'hello',
-	// 		key: 'avatar',
-	// 	});
+		// suinsTx.setDefault('tonysstddondytony.sui');
+		// suinsTx.setUserData({
+		// 	nft,
+		// 	value: 'hello',
+		// 	key: 'avatar',
+		// });
 
-	// 	suinsTx.transaction.transferObjects([nft], suinsTx.getActiveAddress());
-	// }
+		suinsTx.transaction.transferObjects([nft], suinsTx.getActiveAddress());
+	}
 
 	// Creating a subname
 	// const subnameNft = suinsTx.createSubName({
@@ -61,5 +61,5 @@ import { SuinsTransaction } from '../src/suins-transaction';
 	// });
 	// suinsTx.transaction.transferObjects([subnameNft], suinsTx.getActiveAddress());
 
-	// return suinsTx.signAndExecute();
+	return suinsTx.signAndExecute();
 })();
