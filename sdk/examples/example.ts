032b21611068ac7987b143792db23e34241a67e5
Tony Lee <chunglee2018@u.northwestern.edu>
2025-01-17 12:10:14 -0500

    update example

--- sdk/examples/example.ts
+++ sdk/examples/example.ts
@@ -3,6 +3,7 @@
 
 import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
 import { Transaction } from '@mysten/sui/transactions';
+import { MIST_PER_SUI } from '@mysten/sui/utils';
 
 import { SuinsClient } from '../src/suins-client';
 import { SuinsTransaction } from '../src/suins-transaction';
@@ -22,50 +23,50 @@ import { SuinsTransaction } from '../src/suins-transaction';
 	});
 
 	/* Following can be used to fetch the coin type discount, registration price, and renewal price */
-	// console.log(await suinsClient.getPriceList());
-	// console.log(await suinsClient.getRenewalPriceList());
-	// console.log(await suinsClient.getCoinTypeDiscount());
+	console.log(await suinsClient.getPriceList());
+	console.log(await suinsClient.getRenewalPriceList());
+	console.log(await suinsClient.getCoinTypeDiscount());
 
-	// /* Following can be used to fetch the domain record */
-	// console.log('Domain Record: ', await suinsClient.getNameRecord('testing12345.sui'));
+	/* Following can be used to fetch the domain record */
+	console.log('Domain Record: ', await suinsClient.getNameRecord('testing12345.sui'));
 
-	const tx = new Transaction();
-	const coinConfig = suinsClient.config.coins.USDC; // Specify the coin type used for the transaction
-	const priceInfoObjectId =
-		coinConfig !== suinsClient.config.coins.USDC
-			? (await suinsClient.getPriceInfoObject(tx, coinConfig.feed))[0]
-			: null;
-
-	// If discount NFT is used
-	// const discountNft = '0xd047f37bbf8f21dc0a9422c5e99fd208c19bb58884b2671a09c0e1c1cbac6983'; // This can be a string or a kioskTransactionArgument
+	/* If discount NFT is used */
+	// const discountNft = '0xMyDiscountNft'; // This can be a string or a kioskTransactionArgument
 	// const discountNftType = await suinsClient.getObjectType(discountNft);
 
 	/* Registration Example Using SUI */
+	const tx = new Transaction();
 	const suinsTx = new SuinsTransaction(suinsClient, tx);
-	const [coin] = suinsTx.transaction.splitCoins();
+	const maxPaymentAmount = 5 * 1_000_000; // In MIST of the payment coin type
+	const [coin] = suinsTx.transaction.splitCoins('0xMyCoin', [maxPaymentAmount]);
 
-	// /* Registration Example Using USDC */
+	/* Registration Example Using NS */
+	const coinConfig = suinsClient.config.coins.NS; // Specify the coin type used for the transaction
+	const priceInfoObjectId = await suinsClient.getPriceInfoObject(tx, coinConfig.feed)[0];
 	const nft = suinsTx.register({
-		domain: 'namdde1sssa23.sui',
+		domain: 'myname.sui',
 		years: 2,
 		coinConfig,
 		couponCode: 'fiveplus15percentoff',
-		coin: '0x3a6f32a201ad7f7491a56a1f513c97ec24995494019ba02a586fa181c5d266c5',
+		priceInfoObjectId,
+		coin,
 	});
 
-	// /* Registration Example Using NS */
+	/* Registration Example Using USDC */
+	// const coinConfig = suinsClient.config.coins.USDC; // Specify the coin type used for the transaction
 	// const nft = suinsTx.register({
-	// 	domain: 'names123.sui',
+	// 	domain: 'myname.sui',
 	// 	years: 2,
 	// 	coinConfig,
 	// 	couponCode: 'fiveplus15percentoff',
-	// 	coin: '0x8211160f8d782d11bdcfbe625880bc3d944ddb09b4a815278263260b037cd509',
-	// 	priceInfoObjectId,
+	// 	coin,
 	// });
 
 	// /* Renew Example */
+	// const coinConfig = suinsClient.config.coins.SUI; // Specify the coin type used for the transaction
+	// const priceInfoObjectId = await suinsClient.getPriceInfoObject(tx, coinConfig.feed)[0];
 	// suinsTx.renew({
-	// 	nft: '0xde6815eaf725e49e34e8d9422098e71fea787c0278663877650007b83eed10af',
+	// 	nft: '0xMyNft',
 	// 	years: 2,
 	// 	coinConfig,
 	// 	couponCode: 'fiveplus15percentoff',
@@ -74,25 +75,25 @@ import { SuinsTransaction } from '../src/suins-transaction';
 	// });
 
 	/* Optionally set target address */
-	// suinsTx.setTargetAddress({ nft, address: 'YOUR_ADDRESS' });
+	suinsTx.setTargetAddress({ nft, address: 'YOUR_ADDRESS' });
 
-	// /* Optionally set default */
-	// suinsTx.setDefault('name123.sui');
+	/* Optionally set default */
+	suinsTx.setDefault('name123.sui');
 
-	// /* Optionally set user data */
-	// suinsTx.setUserData({
-	// 	nft,
-	// 	value: 'hello',
-	// 	key: 'walrus_site_id',
-	// });
+	/* Optionally set user data */
+	suinsTx.setUserData({
+		nft,
+		value: 'hello',
+		key: 'walrus_site_id',
+	});
 
-	// /* Optionally transfer the NFT */
-	suinsTx.transaction.transferObjects(
-		[nft],
-		'0xb3d277c50f7b846a5f609a8d13428ae482b5826bb98437997373f3a0d60d280e',
-	);
+	/* Optionally transfer the NFT */
+	suinsTx.transaction.transferObjects([nft], '0xMyAddress');
 
-	// /* Subname Example */
+	/* Optionally transfer coin */
+	suinsTx.transaction.transferObjects([coin], '0xMyAddress');
+
+	/* Subname Example */
 	// const subnameNft = suinsTx.createSubName({
 	// 	parentNft: '0x0',
 	// 	name: 'name.name123.sui',
@@ -102,7 +103,7 @@ import { SuinsTransaction } from '../src/suins-transaction';
 	// });
 	// suinsTx.transaction.transferObjects([subnameNft], 'YOUR_ADDRESS');
 
-	// /* Extend Subname Expiration */
+	/* Extend Subname Expiration */
 	// suinsTx.extendExpiration({
 	// 	nft: '0x0',
 	// 	expirationTimestampMs: 1862511339394,
