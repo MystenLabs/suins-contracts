4a966c9047d83990eb5938692472b90840708c7f
SITADRITA1 <mrlime2018@gmail.com>
2025-03-02 15:48:36 +0100

    Update theme.config.jsx

--- documentation/theme.config.jsx
+++ documentation/theme.config.jsx
@@ -9,7 +9,7 @@ export default {
    logo: <>
   <img className="w-[200px] mx-4 inline text-white" src="/logo.svg" alt="Sui Name Service Logo" />
   <span>Sui Name Service Docs</span>
-</>,,
+</>,
     docsRepositoryBase: 'https://github.com/MystenLabs/suins-contracts/tree/main/documentation',
     project: {
       link: 'https://github.com/MystenLabs/suins-contracts'
