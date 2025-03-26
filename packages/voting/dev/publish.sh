8b50147f8640a09c03f0ead4e1eb9f9a73619d45
juzybits <96890594+juzybits@users.noreply.github.com>
2025-03-26 19:09:07 +0100

    dev: update publish script

--- packages/voting/dev/publish.sh
+++ packages/voting/dev/publish.sh
@@ -9,35 +9,81 @@ SCRIPT_DIR="$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )"
 
 ### paths ###
 
-PACKAGES_DIR="$SCRIPT_DIR/../../"
+PACKAGES_DIR="$SCRIPT_DIR/../.."
 TOKEN_DIR="$PACKAGES_DIR/token"
 VOTING_DIR="$PACKAGES_DIR/voting"
+
+### variables ###
+
+TOKEN_PACKAGE_ID=""
+VOTING_PACKAGE_ID=""
+GOVERNANCE_OBJ_ID=""
+STAKING_CONFIG_OBJ_ID=""
+ACTIVE_ENV=$(sui client active-env)
 
 ### functions ###
 
 function publish() {
     local dir="$1"
-
+    echo "================================================"
     echo "Publishing $dir"
     cd "$dir"
     local json=$(sui client publish --json)
     local package_id=$(get_package_id "$json")
     echo "Package ID: $package_id"
+
+    if [ "$dir" == "$TOKEN_DIR" ]; then
+        TOKEN_PACKAGE_ID=$package_id
+    elif [ "$dir" == "$VOTING_DIR" ]; then
+        VOTING_PACKAGE_ID=$package_id
+        GOVERNANCE_OBJ_ID=$(get_object_id "$json" "$package_id" "governance::NSGovernance")
+        STAKING_CONFIG_OBJ_ID=$(get_object_id "$json" "$package_id" "staking_config::StakingConfig")
+
+        echo "NSGovernance ID: $GOVERNANCE_OBJ_ID"
+        echo "StakingConfig ID: $STAKING_CONFIG_OBJ_ID"
+    fi
 }
 
 function get_package_id() {
     local json="$1"
 
-    echo "$json" | jq '.objectChanges[] | select(.type == "published") | .packageId'
+    echo "$json" | jq -r '.objectChanges[] | select(.type == "published") | .packageId'
+}
+
+function get_object_id() {
+    local json="$1"
+    local package_id="$2"
+    local object_type="$3"
+
+    echo "$json" | jq -r ".objectChanges[] | select(.objectType == \"$package_id::$object_type\") | .objectId"
+}
+
+function print_ts_config() {
+    echo
+    echo "  $ACTIVE_ENV: {"
+    echo "    votingPkgId:"
+    echo "      \"$VOTING_PACKAGE_ID\","
+    echo "    governanceObjId:"
+    echo "      \"$GOVERNANCE_OBJ_ID\","
+    echo "    stakingConfigId:"
+    echo "      \"$STAKING_CONFIG_OBJ_ID\","
+    echo "    coinType:"
+    echo "      \"$TOKEN_PACKAGE_ID::ns::NS\","
+    echo "    votingTokenType:"
+    echo "      \"$TOKEN_PACKAGE_ID::ns::NS\","
+    echo "    faucet: \"\","
+    echo "  },"
 }
 
 ### main ###
 
 # prevent from running on mainnet
-if [ "$(sui client active-env)" == "mainnet" ]; then
+if [ "$ACTIVE_ENV" == "mainnet" ]; then
     echo "The active environment is mainnet. Aborting."
     exit 1
 fi
 
 publish "$TOKEN_DIR"
 publish "$VOTING_DIR"
+
+print_ts_config
