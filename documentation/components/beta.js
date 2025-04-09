e761c82387d4d0448c5851e0096a21c8590f934b
Tony Lee <chunglee2018@u.northwestern.edu>
2025-04-09 10:58:07 -0400

    cleanup

--- documentation/components/beta.js
+++ documentation/components/beta.js
@@ -14,8 +14,13 @@ const Beta = () => {
       <div className="mt-6">
         <p className="nx-mt-6 nx-leading-7 first:nx-mt-0">The main point of failure MVR users need to be aware of is the experimental indexer backing the registry. Whether you run your own instance of the MVR indexer or use the Mysten Lab operated public good indexer, there is always risk that the service is not available, so build your application accordingly.</p>
         <p className="nx-mt-6 nx-leading-7 first:nx-mt-0 hidden">
-        Here's how you (can use a fallback address in your typescript to easily fall back to not using MVR / should structure your application so it's easy to maintain uptime in the face of MVR outage / however else users can de-risk using MVR)
+        Read the documentation to learn how to:
         </p>
+        <ul>
+          <li>Use a fallback address in your TypeScript to account for situations when MVR might not be available.</li>
+          <li>Structure your application so it's easier to maintain uptime in the face of MVR outages.</li>
+          <li>Take additional steps to de-risk using MVR.</li>
+        </ul>
       </div> }
     </>
 	);
