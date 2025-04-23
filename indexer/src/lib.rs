23c6d25cfa906eb35c76a11068f07fd1a9846644
patrick <patrickkuo@me.com>
2025-04-23 10:42:54 +0100

    suins indexer using indexer-alt

---
+++ indexer/src/lib.rs
@@ -0,0 +1,10 @@
+use diesel_migrations::{embed_migrations, EmbeddedMigrations};
+
+pub mod handlers;
+
+mod models;
+mod schema;
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
+
+pub const MAINNET_REMOTE_STORE_URL: &str = "https://checkpoint.mainnet.sui.io";
