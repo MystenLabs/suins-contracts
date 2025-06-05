f53629f617203377c5090379af8f06f174670c96
Tony Lee <chunglee2018@u.northwestern.edu>
2025-06-05 15:37:16 -0400

    funds to treasury

--- scripts/transactions/funds_to_treasury.ts
+++ scripts/transactions/funds_to_treasury.ts
@@ -6,19 +6,34 @@ import { Transaction } from '@mysten/sui/transactions';
 import { mainPackage } from '../config/constants';
 import { prepareMultisigTx } from '../utils/utils';
 
-const craftTx = async () => {
+const fundsToTreasury = async () => {
 	const txb = new Transaction();
 	const config = mainPackage.mainnet;
 
 	const adminCapObj = txb.object(config.adminCap);
 
-	const generalProfits = txb.moveCall({
+	const suiProfits = txb.moveCall({
 		target: `${config.packageId}::suins::withdraw`,
 		arguments: [adminCapObj, txb.object(config.suins)],
 	});
 
-	txb.transferObjects([generalProfits], txb.pure.address(config.treasuryAddress!));
+	const nsProfits = txb.moveCall({
+		target: `${config.packageId}::suins::withdraw_custom`,
+		arguments: [txb.object(config.suins), adminCapObj],
+		typeArguments: [config.coins.NS.type],
+	});
+
+	const usdcProfits = txb.moveCall({
+		target: `${config.packageId}::suins::withdraw_custom`,
+		arguments: [txb.object(config.suins), adminCapObj],
+		typeArguments: [config.coins.USDC.type],
+	});
+
+	txb.transferObjects(
+		[suiProfits, nsProfits, usdcProfits],
+		txb.pure.address(config.treasuryAddress!),
+	);
 	await prepareMultisigTx(txb, 'mainnet', config.adminAddress);
 };
 
-craftTx();
+fundsToTreasury();
