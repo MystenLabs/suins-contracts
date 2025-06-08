d3f85170b04eeb1cd2e2815d5d3210db8ea5d61c
juzybits <96890594+juzybits@users.noreply.github.com>
2025-06-08 20:13:51 +0200

    bbb: add dev scripts

---
+++ packages/bbb/scripts/src/find-txs.ts
@@ -0,0 +1,31 @@
+import { getAftermathAmmPkgId, newSuiClient } from "./lib/utils";
+
+const client = newSuiClient();
+
+let txDigests: string[] = [];
+
+// while (txDigests.length === 0) {
+    let cursor: string | null | undefined = null;
+    const txs = await client.queryTransactionBlocks({
+        filter: {
+            MoveFunction: {
+                package: getAftermathAmmPkgId(),
+                module: "swap",
+                function: "swap_exact_in",
+            },
+        },
+        // options: {
+        //     showEffects: true,
+        // },
+        cursor,
+    });
+    cursor = txs.nextCursor;
+    txDigests = txs.data
+        // .filter((tx) => tx.effects?.status.status === "success")
+        .map((tx) => tx.digest);
+//     if (txDigests.length === 0) {
+//         console.debug(`No txs found, fetching next page...`);
+//     }
+// }
+
+console.log(txDigests);
