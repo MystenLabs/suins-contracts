ea4275e36a52c6b9130ba0cd632fe47edaf26ff1
juzybits <96890594+juzybits@users.noreply.github.com>
2025-06-18 21:43:51 +0200

    bbb: additional script config

--- packages/bbb/scripts/src/config.ts
+++ packages/bbb/scripts/src/config.ts
@@ -16,11 +16,19 @@ export const cnf = {
     pyth: {
         endpoint: "https://hermes.pyth.network",
         stateObj: "0x1f9310238ee9298fb703c3419030b35b22bb1cc37113e3bb5007c99aec79e5b8",
+        /** How stale the Pyth price can be, in seconds. */
+        max_age_secs: "60",
     },
     aftermath: {
         ammPackage: "0xc4049b2d1cc0f6e017fda8260e4377cecd236bd7f56a54fee120816e72e2e0dd", // v2
         // aftermathAmmPkgId: "0xefe170ec0be4d762196bedecd7a065816576198a6527c99282a2551aaa7da38c", // v1
         // aftermathAmmPkgId: "0xf948935b111990c2b604900c9b2eeb8f24dcf9868a45d1ea1653a5f282c10e29", // v3
+        pools: {
+            sui_usdc: "0xb0cc4ce941a6c6ac0ca6d8e6875ae5d86edbec392c3333d008ca88f377e5e181",
+            sui_ns: "0xee7a281296e0a316eff84e7ea0d5f3eb19d1860c2d4ed598c086ceaa9bf78c75",
+        },
+        /** Swap slippage tolerance as `1 - slippage` in 18-decimal fixed point. */
+        slippage: "980_000_000_000_000_000", // 2%
     },
     bbb: {
         package: "0x2ec3309b921aa1f819ff566d66bcb3bd045dbaf1fbe58f3141ac6e8f7a9e5d51", // dev-only
@@ -33,14 +41,17 @@ export const cnf = {
         SUI: {
             type: "0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI",
             feed: "0x23d7315113f5b1d3ba7a83604c44b94d79f4fd69af77f804fc7f920a6dc65744",
+            decimals: 9,
         },
         NS: {
             type: "0x5145494a5f5100e645e4b0aa950fa6b68f614e8c59e17bc5ded3495123a79178::ns::NS",
             feed: "0xbb5ff26e47a3a6cc7ec2fce1db996c2a145300edc5acaabe43bf9ff7c5dd5d32",
+            decimals: 6,
         },
         USDC: {
             type: "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC",
             feed: "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a",
+            decimals: 6,
         },
     },
 } as const;
