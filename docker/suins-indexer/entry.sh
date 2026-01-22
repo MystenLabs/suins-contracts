8f3a5a40f687793a0565ff0a09928548084cedee
nikos-kitmeridis <nikos.kitmeridis@mystenlabs.com>
2026-01-22 14:26:22 +0200

    [auction]: add first checkpoint option in the entry script

--- docker/suins-indexer/entry.sh
+++ docker/suins-indexer/entry.sh
@@ -6,4 +6,15 @@ export RUST_LOG=debug
 # for initdb
 export PATH="/usr/lib/postgresql/$(ls /usr/lib/postgresql | head -n 1)/bin:$PATH"
 
-/opt/mysten/bin/suins-indexer --database-url "$DB_URL"
+# Build command with optional arguments
+CMD="/opt/mysten/bin/suins-indexer --database-url $DB_URL"
+
+if [ -n "$FIRST_CHECKPOINT" ]; then
+    CMD="$CMD --first-checkpoint $FIRST_CHECKPOINT"
+fi
+
+if [ -n "$LAST_CHECKPOINT" ]; then
+    CMD="$CMD --last-checkpoint $LAST_CHECKPOINT"
+fi
+
+exec $CMD
